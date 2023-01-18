197129b7a131c388177022b67c70dcc986e9e35f
Max Gaillard <maxgaillar@gmail.com>
2023-01-18 10:18:29 -0500

    create src/api/resources.js file with urls for backend and skryfall api

---
+++ src/api/resources.js
@@ -0,0 +1,9 @@
+// Uniform Resource Locator : address of a given resource on the web
+
+const BASE_URL = 'https://magicfind-server.onrender.com';
+const API_URL = 'https://api.scryfall.com';
+
+export const api = {
+  skryfallURL: API_URL,
+  serverURL: BASE_URL,
+};
