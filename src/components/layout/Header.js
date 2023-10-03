ee7e35043b9af5abd1367a6679741d29cbebe00d
Max Gaillard <maxgaillar@gmail.com>
2023-10-03 06:52:12 -0400

    Added js focus() method on search field when click on search btn

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -31,6 +31,7 @@ const Header = () => {
     if (e.target.classList.contains('search-btn')) {
       document.querySelector('.search-catalog').style.setProperty('width', 'calc(100% - 4rem)');
       document.querySelector('.search-field').style.setProperty('display', 'block');
+      document.querySelector('.search-field').focus();
       // document.querySelector('#search-btn').style.setProperty('display', 'none');
       setAction('display-search')
     }
