776080e3b4d720b9fa72bac8566c1a0e718c340c
Max Gaillard <maxgaillar@gmail.com>
2023-10-11 13:19:09 -0400

    Upgraded react-router-dom 5.3 to 6. Made changes to SearchResultHeader and form.css

--- src/components/views/search/SearchResultHeader.js
+++ src/components/views/search/SearchResultHeader.js
@@ -1,26 +1,66 @@
 import React from 'react';
-import { FiXCircle } from 'react-icons/fi';
+import { Link, useHistory } from 'react-router-dom';
+import { FiArrowLeftCircle } from 'react-icons/fi';
 import capitalizeString from '../../utilities/capitalizeString';
+import styled from 'styled-components';
 
 const SearchResultHeader = ({ cardName, cards, clearSearch }) => {
+    const history = useHistory();
+    console.log(history);
     return (
-        <header className="result-header">
-            <h3 className="result-title">
-                <div className="result-details">
-                    <span>
-                        {capitalizeString(cardName)}
-                    </span>
+        <header className="search-result-header">
+            <div className="search-result-top">
+                {true === 'add-card' ? (
+                    <GoBack to={'/search-api'} title="Back To Search">
+                        <BackIcon>
+                            <FiArrowLeftCircle size={26} />
+                        </BackIcon>
+                        <BackText>Back to Search</BackText>
+                    </GoBack>
+                ) : (
+                    <GoBack to={'/store'} title="Back To Store">
+                        <BackIcon>
+                            <FiArrowLeftCircle size={26} />
+                        </BackIcon>
+                        <BackText>Back to Store</BackText>
+                    </GoBack>
+                )}
+            </div>
+            <div className="search-result-bottom">
+                <h3 className="card-title">
+                    {capitalizeString(cardName)}
+                </h3>
+                <div className="card-quantity">
                     <span>
                         {`${cards.length} 
                  ` + (cards.length > 1 ? 'Results' : 'Result')}
                     </span>
                 </div>
-                <span className="clear-search" onClick={clearSearch}>
+                {/* <span className="clear-search" onClick={clearSearch}>
                     <FiXCircle size={20} />
-                </span>
-            </h3>
+            </span> */}
+
+            </div>
         </header>
     )
 }
+
+const GoBack = styled(Link)`
+  display: flex;
+  align-items: center;
+
+  &:hover {
+    opacity: 0.8;
+  }
+`;
+const BackIcon = styled.span`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+const BackText = styled.span`
+  margin-left: 20px;
+`;
 
 export default SearchResultHeader;
