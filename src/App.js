c84f9aaaf223e3325b0083f915210f6044771e78
Max Gaillard <maxgaillar@gmail.com>
2023-12-04 15:49:59 -0500

    changed app.js and package.json

--- src/App.js
+++ src/App.js
@@ -38,12 +38,12 @@ const App = () => {
 
         {/* Public routes */}
         <Route exact path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/shopping-cart" element={<ShoppingCart />} />
-        <Route path="/search-result/:cardName" element={<SearchResult />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<Signup />} />
+        <Route path="shopping-cart" element={<ShoppingCart />} />
+        <Route path="search-result/:cardName" element={<SearchResult />} />
 
         {/* Auth protected routes */}
         <Route element={<RequireAuth />}>
