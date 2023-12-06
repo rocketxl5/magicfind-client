d1cc342da6a7832dc5ff8b434a1658f4b4ab0dd4
Max Gaillard <maxgaillar@gmail.com>
2023-12-06 11:34:07 -0500

    Removed overflow from Modal to SearchResult

--- src/components/views/search/Modal.js
+++ src/components/views/search/Modal.js
@@ -2,11 +2,7 @@ import { createPortal } from 'react-dom';
 
 const Modal = ({ open, children }) => {
     if (!open) {
-        document.body.classList.remove('overflow-hidden');
         return null;
-    }
-    else {
-        document.body.classList.add('overflow-hidden');
     }
 
     return createPortal(
