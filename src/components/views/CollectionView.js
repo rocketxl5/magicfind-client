2eea98800810c1b80d618f12f1da7385a2cc079f
Max Gaillard <maxgaillar@gmail.com>
2023-12-25 10:49:51 -0500

    testing height for mobile 2

--- src/components/views/CollectionView.js
+++ src/components/views/CollectionView.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from 'react';
+import SearchCollection from './search/SearchCollection';
+import SearchApi from './search/SearchAPI';
 
 const CollectionView = () => {
     return (
         <div className="content">
             Collection
-
+            <SearchCollection />
+            <SearchApi />
         </div>
     )
 }
