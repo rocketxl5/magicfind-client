635e58dcaf6574fa1f0a8af05c85890ec8559e16
Max Gaillard <maxgaillar@gmail.com>
2023-12-31 15:43:33 -0500

    Styling landing page

--- src/components/views/About.js
+++ src/components/views/About.js
@@ -1,16 +1,36 @@
 import React from 'react';
+import frank from '../../assets/img/frank.png'
 
 const About = () => {
-  return (
-    <div className="content">
-      <header className="header">
+  return (<>
+    {/* <section className="features-section about-me">
+      <div className="col-3 img-container">
+        <img src={frank} alt="Frankenstein" />
+      </div>
+      <div className="feature col-9">
 
-        <h2 className="title">About</h2>
-      </header>
-      <main className="main">
+      </div>
+    </section>
+    <section className="features-section about-me">
+      <div className="feature col-8">
 
-      </main>
-    </div>
+      </div>
+      <div className="col-4 quarter-green">
+      </div>
+    </section>
+    <section className="features-section about-me">
+      <div className="col-2 fifth-square mtg-blue">
+      </div>
+      <div className="col-2 fifth-square mtg-green">
+      </div>
+      <div className="col-2 fifth-square mtg-red">
+      </div>
+      <div className="col-2 fifth-square mtg-white">
+      </div>
+      <div className="col-2 fifth-square mtg-black">
+      </div>
+    </section> */}
+  </>
   );
 };
 
