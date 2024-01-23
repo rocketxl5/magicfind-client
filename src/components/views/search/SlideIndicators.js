94f5766859f70ba84cf3431e7af7db98ccc1fd7b
Max Gaillard <maxgaillar@gmail.com>
2024-01-23 09:19:49 -0500

    Added indicators component for slider

---
+++ src/components/views/search/SlideIndicators.js
@@ -0,0 +1,26 @@
+import { FaRegCircle, FaSatelliteDish } from "react-icons/fa6";
+import { FaRegCircleDot } from "react-icons/fa6";
+
+const SlideIndicators = ({ slides, currentIndicator }) => {
+    console.log(slides)
+    console.log(currentIndicator)
+    return (
+        <div className="indicators">
+            {
+                slides &&
+                slides.map((slide, index) => {
+                    return (
+                        // <span key={index} className={`indicator ${currentIndicator === index && 'current-indicator'}`}>
+
+                        <span key={index} className="indicator">
+                            {currentIndicator === index ? <FaRegCircleDot /> : <FaRegCircle />}
+                        </span>
+                    )
+                })
+
+            }
+        </div>
+    )
+}
+
+export default SlideIndicators
