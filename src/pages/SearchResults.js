72654fa6bd7fce22de05b3fff496c9f47bc741a1
Max Gaillard <maxgaillar@gmail.com>
2024-03-12 15:07:46 -0400

    Try fix cataloghandler error: removed console.log of undefined  @ search results

--- src/pages/SearchResults.js
+++ src/pages/SearchResults.js
@@ -1,14 +1,12 @@
-import { useState, useEffect, useContext } from 'react';
+import { useState, useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import ProductItem from '../features/product/ProductItem';
 import Modal from '../features/modal/Modal';
 import Page from '../components/Page';
 import Count from '../features/search/components/Count';
-import { NavbarContext } from '../contexts/NavbarContext';
 import SearchParameters from '../features/search/components/SearchParameters';
 import useProductForms from '../hooks/useProductForms';
 import useSlideView from '../hooks/useSlideView';
-import useNavbar from '../hooks/contexthooks/useNavbar';
 
 const SearchResults = () => {
     // States
@@ -17,16 +15,9 @@ const SearchResults = () => {
     const location = useLocation();
     const navigate = useNavigate();
 
-    const { displaySeachBar, setDisplaySearchBar } = useContext(NavbarContext);
-
     const { cards, search } = location.state?.result || JSON.parse(localStorage.getItem('search-results'));
 
     useEffect(() => {
-        console.log(displaySeachBar)
-        // if (displaySeachBar) {
-        //     // console.log(displaySeachBar)
-        //     setDisplaySearchBar(false);
-        // }
         // If cards is empty
         if (!cards.length) {
             // Send to collection view
