4e6dea734b424812469120fc8c144cf5ea1c467c
Max Gaillard <maxgaillar@gmail.com>
2024-04-10 16:58:42 -0400

    Try fix flip btn: Reduced z-index of rotate-180 class

--- src/features/modal/TwoSidedSlide.js
+++ src/features/modal/TwoSidedSlide.js
@@ -11,21 +11,18 @@ const TwoSidedSlide = ({ children, classList, motion }) => {
 
     return (
         // <div id={motion} className={classList.container} >
-        <>
+        <div className={`slide ${classList.container}`}>
             {isMounted && <TurnBtn classList={classList.btn} target={cardRef.current} />}
-            <div className={`slide ${classList.container}`}>
-                <div className="double-faced-card" ref={cardRef}>
-                    <div className="double-faced-recto">
-                        {children[0]}
-                    </div>
-                    <div className="double-faced-verso">
-                        {children[1]}
-                    </div>
+            <div className="double-faced-card" ref={cardRef}>
+                <div className="double-faced-recto">
+                    {children[0]}
+                </div>
+                <div className="double-faced-verso">
+                    {children[1]}
                 </div>
             </div>
-        </>
+        </div>
         // </div>
-
     )
 }
 
