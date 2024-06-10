59803692bf29d4de3fedc1b4c32b5e53c289cf81
Max Gaillard <maxgaillar@gmail.com>
2024-06-10 09:04:02 -0400

    Removed imagesLoaded conditionnal for set icon display @ Set component

--- src/features/search/components/Set.js
+++ src/features/search/components/Set.js
@@ -6,7 +6,7 @@ const Set = ({ set }) => {
 
     const { cardSets } = useSearch();
 
-    const { imagesLoaded, setUris } = useImageLoader(); 
+    const { setUris } = useImageLoader(); 
 
     useEffect(() => {
         setUris(set.prints.map(print => print?.image_uris ? print?.image_uris.normal : print?.card_faces[0].image_uris.normal))
@@ -16,11 +16,11 @@ const Set = ({ set }) => {
         <div className="set">
             <div className="set-header">
                 <h2>
-                    {
-                        imagesLoaded &&
-                        <span className='set-icon' style={{ maskImage: `url(${cardSets[set.id]?.icon_svg_uri})`, WebkitMaskImage: `url(${cardSets[set.id]?.icon_svg_uri})` }}>
-                        </span>
-                    }
+                    <span
+                        className='set-icon'
+                        style={{ maskImage: `url(${cardSets[set.id]?.icon_svg_uri})`, WebkitMaskImage: `url(${cardSets[set.id]?.icon_svg_uri})` }}
+                    >
+                    </span>
                     <span className='set-name'>{cardSets[set.id]?.name}</span>
                 </h2>
             </div>
