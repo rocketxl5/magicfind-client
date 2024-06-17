c6361bef5ba30503917f5e053a005aa387750184
Max Gaillard <maxgaillar@gmail.com>
2024-06-16 23:20:30 -0400

    Replaced and improved modal icon css. Removed style components from Logo Component

--- src/contexts/IconContext.js
+++ src/contexts/IconContext.js
@@ -1,12 +1,21 @@
 // source @ https://github.com/react-icons/react-icons#configuration
+import { createContext } from 'react';
 import { IconContext } from 'react-icons';
 
-export const IconProvider = ({ children }) => {
+// export const ReactIconsContext = createContext(null);
 
+export const IconProvider = ({ children }) => {
+    // stroke fill strokeWidth heigth width
     return (
         <IconContext.Provider
             value={{
-                style: { verticalAling: 'middle' }
+                style: {
+                    verticalAlign: 'middle',
+                    stroke: 'rgb(219, 219, 219)',
+
+                    width: '100%',
+                    height: '100%'
+                }
             }}
         >
             {children}
